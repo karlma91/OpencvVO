8e261ab460c6a325608c755ce207f249c9a69da0
Kai Wig <kaiwig91@gmail.com>
2015-05-15 15:25:06 +0200

    La til viz visualisering av kameraposisjon/retning

--- OpenCVTest/Tests.h
+++ OpenCVTest/Tests.h
@@ -1,11 +1,15 @@
 #ifndef _A_H // must be unique name in the project
 #define _A_H
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
 
 int RunOrb();
 int PyrLKTest();
 int PyrLKod();
+int cameraPose();
 
 #endif 
 
--- OpenCVTest/main.cpp
+++ OpenCVTest/main.cpp
@@ -4,5 +4,6 @@
 int main() {
 	//PyrLKTest();
 	//RunOrb();
-	PyrLKod();
+  PyrLKod();
+  //      cameraPose();
 }
